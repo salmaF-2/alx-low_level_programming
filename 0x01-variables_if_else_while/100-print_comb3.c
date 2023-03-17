3092760004c9bf53df6efbeb81a0377c6bcb1fe8
salma alfadili <salmafadili003@gmail.com>
2023-03-17 15:14:04 -0700

    task10 done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,17 +6,19 @@
  */
 int main(void)
 {
-int i, j;
-for (i = 0 ; i < 10 ; i++)
+int i;
+int j;
+for (i = 0 ; i < 9 ; i++)
 {
-for (j = i + 1 ; j < 10 ; j++)
+for (j = i + 1 ; j <= 9 ; j++)
 {
 putchar('0' + i);
 putchar('0' + j);
-if (i == 9 && j == 9)
-continue;
+if (i != 8 || j != 9)
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
