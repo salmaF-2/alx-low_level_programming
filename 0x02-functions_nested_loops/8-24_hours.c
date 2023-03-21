88c959bba744f73edd6d0ded78188e2a5315a62d
salma alfadili <salmafadili003@gmail.com>
2023-03-21 13:53:33 -0700

    task8 done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+int h, min;
+for (h = 0; h <= 23; h++)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar(h / 10 + 48);
+_putchar(h % 10 + 48);
+_putchar(58);
+_putchar(min / 10 + 48);
+_putchar(min % 10 + 48);
+_putchar('\n');
+}
+}
+}
