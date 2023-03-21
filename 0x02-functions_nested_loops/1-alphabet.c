7adb9ad3e882a2d5d3629e9b9a77ab20767878d4
salma alfadili <salmafadili003@gmail.com>
2023-03-21 06:27:37 -0700

    task1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
