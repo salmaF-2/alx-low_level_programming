8be24b966c6cd5ccc522f827eeda58e529ca777c
salma alfadili <salmafadili003@gmail.com>
2023-03-21 15:11:37 -0700

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: first number
+ * @b: second number
+ * Return:  the result.
+ */
+int add(int a, int b)
+{
+int result = a + b;
+return (result);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,26 +6,30 @@
 void times_table(void)
 {
 int i, j;
-for (i = 0; i < 10; i++)
+for (i = 0; i <= 9; i++)
 {
-for (j = 0; j < 10; j++)
+for (j = 0; j <= 9; j++)
 {
-int m = i * j;
+int m = j * i;
 if (j == 0)
 {
-_putchar(48);
-}
-if (m < 10)
+if (i == 0)
 {
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
 _putchar(m + 48);
+}
+else
+{
+_putchar(m + 48);
+}
 }
 else
 {
 _putchar(',');
 _putchar(' ');
+if (m <= 9)
+{
+_putchar(' ');
+}
 _putchar(m / 10 + 48);
 _putchar(m % 10 + 48);
 }
