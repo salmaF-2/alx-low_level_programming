80effc01dc2840d8fd18814f04d984012db2a99f
salma alfadili <salmafadili003@gmail.com>
2023-03-21 17:26:10 -0700

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,14 +6,17 @@
 int main(void)
 {
 int count = 0;
-int a = 1, b = 2;
+int a = 1;
+int b = 2;
 printf("%d, %d, ", a, b);
 count += 2;
 while (count < 98)
+{
 int next = a + b;
 printf("%d", next);
 count++;
 if (count != 98)
+{
 printf(", ");
 }
 a = b;
