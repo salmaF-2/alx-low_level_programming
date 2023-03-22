ecaf3016f1cf54ec744c0b929dfd1b535437a2c0
salma alfadili <salmafadili003@gmail.com>
2023-03-21 17:40:26 -0700

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
+#define LARGEST 10000000000
 /**
  * main - main function
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int count = 0;
-int a = 0;
-int b = 1;
-printf("%d, %d, ", a, b);
-count += 2;
-while (count < 100)
+unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+unsigned long int hold1, hold2, hold3;
+int count;
+printf("%lu, %lu, ", bk1, bk2);
+for (count = 2; count < 98; count++)
 {
-int next = a + b;
-printf("%d", next);
-count++;
-if (count != 100)
+if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
 {
-printf(", ");
+hold1 = (bk1 + bk2) / LARGEST;
+hold2 = (bk1 + bk2) % LARGEST;
+hold3 = fr1 + fr2 + hold1;
+fr1 = fr2, fr2 = hold3;
+bk1 = bk2, bk2 = hold2;
+printf("%lu%010lu", fr2, bk2);
 }
-a = b;
-b = next;
+else
+{
+hold2 = bk1 + bk2;
+bk1 = bk2, bk2 = hold2;
+printf("%lu", bk2);
+}
+if (count != 97)
+printf(", ");
 }
 printf("\n");
 return (0);
