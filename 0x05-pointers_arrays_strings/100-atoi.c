9442013fae24ec29d8ca663b9c96d775c7ce266e
salma alfadili <salmafadili003@gmail.com>
2023-03-27 19:26:37 -0700

    task10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer.
+ * @s:string to convert to character
+ * Return: to integer
+ */
+int _atoi(char *s)
+{
+int sign = 1;
+int num = 0;
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+{
+sign = -1;
+}
+else if (s[i] == '+')
+{
+sign = 1;
+}
+else if (s[i] >= '0' && s[i] <= '9')
+{
+num = num * 10 + (s[i] - '0');
+}
+else if (num > 0)
+{
+break;
+}
+i++;
+}
+return (num  *sign);
+}
