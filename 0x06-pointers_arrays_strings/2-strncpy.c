61d4ccc438c8ed6758896e5b7405de9fc6d9e0f8
salma alfadili <salmafadili003@gmail.com>
2023-03-29 18:20:39 -0700

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,10 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
-while (i < n && *src != '\0')
-{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
 *(dest + i) = *(src + i);
-i++;
-}
-while (i < n)
-{
+for ( ; i < n; i++)
 *(dest + i) = '\0';
-i++;
-}
 return (dest);
 }
