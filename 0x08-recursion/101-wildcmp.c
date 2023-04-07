bcdd941c794060892816d949f1abd2ba5080ecf2
salma alfadili <salmafadili003@gmail.com>
2023-04-06 19:44:39 -0700

    done

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,8 +8,24 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-if (*s1 == '\0' && *s2 == '\0')
+if (*s2 == '*')
+{
+if (wildcmp(s1, s2 + 1) == 1)
 {
 return (1);
 }
-
+return (wildcmp(s1 + 1, s2));
+}
+else if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
+else if (*s1 == '\0' && *s2 != '\0')
+{
+return (0);
+}
+else
+{
+return (0);
+}
+}
