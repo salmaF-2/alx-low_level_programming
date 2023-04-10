df337da706af577700c3f8a025729ca75e7dcb81
salma alfadili <salmafadili003@gmail.com>
2023-04-09 20:50:49 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: the number of arguments passed 
+ * @argv: array represents the actual arguments passed to the program
+ * Return: 0 (success) 
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+(void)argc;
+return (0);
+}
