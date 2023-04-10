47392e6e9ab6401907e5fb11a662169f1b247fa9
salma alfadili <salmafadili003@gmail.com>
2023-04-09 20:52:33 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * main - prints its name, followed by a new line.
- * @argc: the number of arguments passed 
+ * @argc: the number of arguments passed
  * @argv: array represents the actual arguments passed to the program
- * Return: 0 (success) 
+ * Return: 0 (success)
  */
 int main(int argc, char *argv[])
 {
