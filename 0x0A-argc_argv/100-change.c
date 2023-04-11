e7b705bd6e6cf1a6f69d03421907531d408a70af
salma alfadili <salmafadili003@gmail.com>
2023-04-10 20:01:32 -0700

    done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -19,22 +19,22 @@ return (1);
 cash = atoi(argv[1]);
 if (cash <= 0)
 {
-	printf("0\n");
-			return (0);
-				}
-	for (count = 0; cash > 0; count++)
-		{
-					if (cash >= 25)
-									cash = cash - 25;
-							else if (cash >= 10 && cash < 25)
-											cash = cash - 10;
-									else if (cash >= 5 && cash < 10)
-													cash = cash - 5;
-											else if (cash >= 2 && cash < 5)
-															cash = cash - 2;
-													else
-																	cash = cash - 1;
-														}
-	printf("%d\n", count);
-		return (0);
-		}
+printf("0\n");
+return (0);
+}
+for (count = 0; cash > 0; count++)
+{
+if (cash >= 25)
+cash = cash - 25;
+else if (cash >= 10 && cash < 25)
+cash = cash - 10;
+else if (cash >= 5 && cash < 10)
+cash = cash - 5;
+else if (cash >= 2 && cash < 5)
+cash = cash - 2;
+else
+cash = cash - 1;
+}
+printf("%d\n", count);
+return (0);
+}
