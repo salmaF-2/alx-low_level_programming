bd191cc474ad2f318ac86e3e3777af6d981ddac7
salma alfadili <salmafadili003@gmail.com>
2023-04-10 19:29:41 -0700

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - adds positive numbers.
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int i, sum = 0;
+if (argc < 2)
+{
+printf("0\n");
+return (0);
+}
+for (i = 1; i < argc; i++)
+{
+int num = atoi(argv[i]);
+if (!isdigit(*argv[i]) || num <= 0)
+{
+printf("Error\n");
+return (1);
+}
+sum = sum + num;
+}
+printf("%i\n", sum);
+return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 int _putchar(char c);
 
 
