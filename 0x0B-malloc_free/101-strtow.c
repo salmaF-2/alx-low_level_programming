b2ee69d388a3ba2eb7f74e3e1851ce2ebc69a61a
salma alfadili <salmafadili003@gmail.com>
2023-04-11 19:33:12 -0700

    task0

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * strtow - function that splits a string into words.
+ * @str: string
+ * Return: to pointer to array or NULL if str == NULL or str == ""
+ */
+char **strtow(char *str)
+{
+
+
+
+
+
+
+
+
+}
