0f4e5e78cbe4812b61f13f3ebffe1f3c448f10dd
salma alfadili <salmafadili003@gmail.com>
2023-04-19 19:19:38 -0700

    not yet

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - function that prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ * Return: always O
+ */
+int main(int argc, char **argv)
+{
+
+}
