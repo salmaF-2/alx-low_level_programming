d3352e59cb6bc50dd5fb991a1496afab960b8943
salma alfadili <salmafadili003@gmail.com>
2023-04-19 17:22:25 -0700

    task3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - function that Prints result of an operation
+ * @argc: Number of arguments
+ * @argv: Value of arguments
+ * Return: Returns an int.
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+int (*op_func)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+op_func = get_op_func(argv[2]);
+if (op_func == NULL)
+{
+printf("Error\n");
+exit(98);
+}
+result = op_func(num1, num2);
+printf("%d\n", result);
+return (0);
+}
