d1937e99ba3f40e48769e434bbc319d45ead1b1b
salma alfadili <salmafadili003@gmail.com>
2023-04-20 22:04:48 -0700

    task3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,17 +1,5 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-#include <stdarg.h>
-/**
- * struct format - Struct format
- *
- * @type: Type of argument
- * @func: Pointer to function to print argument
- */
-typedef struct format
-{
-char type;
-void (*func)(va_list);
-} format_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
