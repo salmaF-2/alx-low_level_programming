776e504631551388e085d8138a10c401dd7630c9
salma alfadili <salmafadili003@gmail.com>
2023-04-20 20:58:34 -0700

    d

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,92 +3,97 @@
 #include <stdarg.h>
 #include <stdlib.h>
 /**
- * struct variable_type - Struct variable_type
- * @type: The data type
- * @f: The function pointer
- */
-typedef struct variable_type
-{
-char type;
-void (*f)(va_list);
-} variable_type;
-
-/**
- * printf_c - print a character.
- * @arg_variables: list of arguments.
- * Return: void
- */
-void printf_c(va_list arg_variables)
-{
-printf("%c", va_arg(arg_variables, int));
-}
-/**
- * printf_i - print an integer.
- * @arg_variables: arguments
- * Return: void
- */
-void printf_i(va_list arg_variables)
-{
-printf("%i", va_arg(arg_variables, int));
-}
-/**
- * printf_f - print a float.
+ * print_all - Prints anything
  *
- * @arg_variables: list of arguments.
- */
-void printf_f(va_list arg_variables)
-{
-printf("%f", va_arg(arg_variables, double));
-}
-/**
- * printf_s - print a string.
- * @arg_variables: arguments
- * Return: void
- */
-void printf_s(va_list arg_variables)
-{
-char *p;
-p = va_arg(arg_variables, char *);
-if (p == NULL)
-p = "(nil)";
-printf("%s", p);
-}
-/**
- * print_all - prints all.
- * @format: last argument.
+ * @format: List of types of arguments passed to the function
+ *
  * Return: void
  */
 void print_all(const char * const format, ...)
 {
-int i = 0;
-int j = 0;
-char *sep = "";
-va_list arg_variables;
-variable_type var[] = {
-{'c', printf_c},
-{'i', printf_i},
-{'f', printf_f},
-{'s', printf_s},
-{'\0', NULL}
-};
-va_start(arg_variables, format);
-while (format && format[i])
-{
-j = 0;
-while (var[j].type)
-{
-if (format[i] == var[j].type)
-{
-printf("%s", sep);
-var[i].f(arg_variables);
-sep = ", ";
-break;
-}
-j++;
-}
-i++;
-}
-printf("\n");
-va_end(arg_variables);
+	int i = 0, j = 0;
+	char *separator = "";
+	va_list args;
+	format_t formats[] = {
+		{'c', print_c},
+		{'i', print_i},
+		{'f', print_f},
+		{'s', print_s},
+		{'\0', NULL}
+	};
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (formats[j].type)
+		{
+			if (format[i] == formats[j].type)
+			{
+				printf("%s", separator);
+				formats[j].func(args);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	printf("\n");
+	va_end(args);
 }
 
+/**
+ * print_c - Prints a char
+ *
+ * @arg: va_list argument pointer
+ *
+ * Return: void
+ */
+void print_c(va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+
+/**
+ * print_i - Prints an int
+ *
+ * @arg: va_list argument pointer
+ *
+ * Return: void
+ */
+void print_i(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+
+/**
+ * print_f - Prints a float
+ *
+ * @arg: va_list argument pointer
+ *
+ * Return: void
+ */
+void print_f(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+
+/**
+ * print_s - Prints a string
+ *
+ * @arg: va_list argument pointer
+ *
+ * Return: void
+ */
+void print_s(va_list arg)
+{
+	char *str = va_arg(arg, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+
+	printf("%s", str);
+}
