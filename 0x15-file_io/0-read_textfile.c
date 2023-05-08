4a99e5dbaed62b45e2bc20262ab52be44288635b
salma alfadili <salmafadili003@gmail.com>
2023-05-08 16:29:15 -0700

    task0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * read_textfile - function reads text file,prints it to POSIX
+ * @filename: name of file
+ * @letters:  number of letters it should read and print
+ * Return: number of letters it could read and print or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+FILE *F = fopen(filename, "r");
+char *buffer = (char*) malloc((letters + 1) * sizeof(char));
+ssize_t total_letters = 0;
+size_t readl;
+if (filename == NULL)
+{
+return (0);
+}
+if (F == NULL)
+{
+return (0);
+}
+if (buffer == NULL)
+{
+fclose(F);
+return (0);
+}
+while ((readl = fread(buffer, sizeof(char), letters, F)) > 0)
+{
+if (fwrite(buffer, sizeof(char), readl,
+fdopen(STDOUT_FILENO, "w")) != readl)
+{
+free(buffer);
+fclose(F);
+return (0);
+}
+total_letters += readl;
+}
+if (ferror(F))
+{
+fclose(F);
+free(buffer);
+return (0);
+}
+fclose(F);
+free(buffer);
+return (total_letters);
+}
